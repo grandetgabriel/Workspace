af864a0dc01cba2aaac83e0447713df9a9dcf88a
Lincoln <grandetgabriel@gmail.com>
2018-09-23 23:46:01 +0200

    Ajouté interface metier

---
+++ src/metier/CreditMetierImpl.java
@@ -0,0 +1,11 @@
+package metier;
+
+public class CreditMetierImpl implements ICreditMetier {
+
+	@Override
+	public double calculerMensualiteCredit(double capital, double taux, int duree) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
