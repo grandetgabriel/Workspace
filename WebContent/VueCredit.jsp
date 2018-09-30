b5b5cc9753761a08b3f196a9bdf94220f214776f
Lincoln <grandetgabriel@gmail.com>
2018-09-30 21:12:34 +0200

    Completer JSP

--- WebContent/VueCredit.jsp
+++ WebContent/VueCredit.jsp
@@ -4,6 +4,29 @@
 <meta charset="utf-8">
 <title>Crédit bancaire</title>
 </head>
-<body>Test
+<body>
+	<div>
+		<form action="calculerMensualite.do" method="post">
+			<table>
+				<tr>
+					<td>Montant:</td>
+					<td><input type="text" name="montant" /></td>
+					<td>euros</td>
+				</tr>
+
+				<tr>
+					<td>Taux:</td>
+					<td><input type="text" name="taux" /></td>
+					<td>%</td>
+				</tr>
+				<tr>
+					<td>Durée:</td>
+					<td><input type="text" name="duree" /></td>
+					<td>mois</td>
+				</tr>
+			</table>
+			<button type="submit">Calculer</button>
+		</form>
+	</div>
 </body>
 </html>
--- src/web/ControlerServlet.java
+++ src/web/ControlerServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "cs", urlPatterns = { "/controleur", "*.php" })
+@WebServlet(name = "cs", urlPatterns = { "/controleur", "*.do" })
 public class ControlerServlet extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
@@ -19,6 +19,15 @@ public class ControlerServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.getRequestDispatcher("VueCredit.jsp").forward(request, response);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		double montant = Double.parseDouble(request.getParameter("montant"));
+		double taux = Double.parseDouble(request.getParameter("taux"));
+		int duree = Integer.parseInt(request.getParameter("duree"));
 
 	}
 
